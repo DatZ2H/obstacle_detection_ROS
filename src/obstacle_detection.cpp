ac963508fb7f571c9089bcb275401345f1659e99
DatZ2H <quocdatdqd.cdt@gmail.com>
2023-06-28 18:29:05 +0700

    demo

--- src/obstacle_detection.cpp
+++ src/obstacle_detection.cpp
@@ -9,6 +9,10 @@
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl_ros/transforms.h>
 #include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
 
 class ObstacleDetection
 {
@@ -25,31 +29,31 @@ public:
 
   void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   {
-    // Chuyển đổi từ sensor_msgs::PointCloud2 sang pcl::PointCloud<pcl::PointXYZRGB>
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    // Chuyển đổi từ sensor_msgs::PointCloud2 sang pcl::PointCloud<pcl::PointXYZ>
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*cloud_msg, *cloud);
 
     // Bước 1: Lọc điểm dữ liệu trong một khoảng giá trị cho trước
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     passThroughFilter(cloud, filtered_cloud);
     filtered_pub_.publish(convertToROSMsg(filtered_cloud));
 
     // Bước 2: Giảm độ phân giải của điểm dữ liệu
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr downsampled_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     voxelGridFilter(filtered_cloud, downsampled_cloud);
     downsampled_pub_.publish(convertToROSMsg(downsampled_cloud));
 
     // Bước 3: Loại bỏ sàn nhà
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr floor_removed_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr floor_removed_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     removeFloor(downsampled_cloud, floor_removed_cloud);
     obstacles_pub_.publish(convertToROSMsg(floor_removed_cloud));
 
     // Bước 4: Phân nhóm các điểm dữ liệu theo cụm
-    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters;
     euclideanClustering(floor_removed_cloud, clusters);
 
     // Xuất các cụm điểm dữ liệu
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr clustered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr clustered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     for (const auto& cluster : clusters)
     {
       *clustered_cloud += *cluster;
@@ -64,33 +68,32 @@ private:
   ros::Publisher downsampled_pub_;
   ros::Publisher obstacles_pub_;
   ros::Publisher clustered_pub_;
-  tf::TransformListener tf_listener_;
 
-  void passThroughFilter(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
-                         pcl::PointCloud<pcl::PointXYZRGB>::Ptr& filtered_cloud)
+  void passThroughFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud,
+                         pcl::PointCloud<pcl::PointXYZ>::Ptr& filtered_cloud)
   {
-    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pcl::PassThrough<pcl::PointXYZ> pass;
     pass.setInputCloud(input_cloud);
     pass.setFilterFieldName("z");
     pass.setFilterLimits(0.0, 2.0);
     pass.filter(*filtered_cloud);
   }
 
-  void voxelGridFilter(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
-                       pcl::PointCloud<pcl::PointXYZRGB>::Ptr& downsampled_cloud)
+  void voxelGridFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud,
+                       pcl::PointCloud<pcl::PointXYZ>::Ptr& downsampled_cloud)
   {
-    pcl::VoxelGrid<pcl::PointXYZRGB> voxel_grid;
+    pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
     voxel_grid.setInputCloud(input_cloud);
     voxel_grid.setLeafSize(0.01, 0.01, 0.01);
     voxel_grid.filter(*downsampled_cloud);
   }
 
-void removeFloor(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
-                 pcl::PointCloud<pcl::PointXYZRGB>::Ptr& floor_removed_cloud)
+void removeFloor(const pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud,
+                 pcl::PointCloud<pcl::PointXYZ>::Ptr& floor_removed_cloud)
 {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-  pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
   pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
   pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
 
@@ -121,14 +124,14 @@ void removeFloor(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
 }
 
 
-  void euclideanClustering(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
-                           std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>& clusters)
+  void euclideanClustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud,
+                           std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr>& clusters)
   {
-    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     tree->setInputCloud(input_cloud);
 
     std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
     ec.setClusterTolerance(0.02);
     ec.setMinClusterSize(100);
     ec.setMaxClusterSize(25000);
@@ -138,7 +141,7 @@ void removeFloor(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
 
     for (const auto& indices : cluster_indices)
     {
-      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster(new pcl::PointCloud<pcl::PointXYZRGB>);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cluster(new pcl::PointCloud<pcl::PointXYZ>);
       for (const auto& index : indices.indices)
       {
         cluster->push_back((*input_cloud)[index]);
@@ -147,7 +150,7 @@ void removeFloor(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
     }
   }
 
-  sensor_msgs::PointCloud2 convertToROSMsg(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud)
+  sensor_msgs::PointCloud2 convertToROSMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
   {
     sensor_msgs::PointCloud2 cloud_msg;
     pcl::toROSMsg(*cloud, cloud_msg);
@@ -159,8 +162,11 @@ void removeFloor(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& input_cloud,
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "obstacle_detection");
+  
+  
 
   ObstacleDetection obstacle_detection;
+
 
   ros::spin();
 
