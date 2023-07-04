03fe9f4106d7ba52817b702e7dcd9d5df7258402
DatZ2H <quocdatdqd.cdt@gmail.com>
2023-07-04 15:23:40 +0700

    Safety oke

--- src/obstacle_detection.cpp
+++ src/obstacle_detection.cpp
@@ -15,10 +15,13 @@
 #include <std_msgs/String.h>
 #include <pcl/point_cloud.h>
 
-
-
-
-
+#include <pcl/search/kdtree.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
+#include <string>
 
 class ObstacleDetection
 {
@@ -54,9 +57,14 @@ public:
     nh_.param<double>("safety_protect_position_x", safety_protect_position_(0), 0.0);
     nh_.param<double>("safety_protect_position_y", safety_protect_position_(1), 0.0);
     nh_.param<double>("safety_protect_position_z", safety_protect_position_(2), 1.0);
+    nh_.param<double>("collision_threshold", collision_threshold_, 0.1);
+    nh_.param<int>("collision_detection_limit", collision_detection_limit_, 5);
+    collision_counter_.resize(collision_detection_limit_, 0);
+
+    displayParameters();
   }
 
-  void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+  void cloudCallback(const sensor_msgs::PointCloud2ConstPtr &cloud_msg)
   {
     // Chuyển đổi sensor_msgs/PointCloud2 thành pcl::PointCloud<pcl::PointXYZ>
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -85,6 +93,8 @@ public:
     clustered_pub_.publish(convertToPointCloud2(clusters[0]));
 
     // Kiểm tra vùng an toàn
+    // Tạo cloud chứa các cụm vật thể
+    pcl::PointCloud<pcl::PointXYZ>::Ptr obstacles_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr safety_warn_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr safety_protect_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     computeSafetyZone(ground_removed_cloud, safety_warn_cloud, safety_protect_cloud);
@@ -92,16 +102,20 @@ public:
     // Gửi thông tin vùng an toàn và trạng thái an toàn
     safety_warn_pub_.publish(convertToPointCloud2(safety_warn_cloud));
     safety_protect_pub_.publish(convertToPointCloud2(safety_protect_cloud));
+
+    // Tính toán trạng thái an toàn
     std_msgs::String safety_status_msg;
-    safety_status_msg.data = computeSafetyStatus(safety_warn_cloud, safety_protect_cloud);
+    computeSafetyStatus(ground_removed_cloud, safety_warn_cloud, safety_protect_cloud, safety_status_msg);
+
+    // Publish trạng thái an toàn
     safety_status_pub_.publish(safety_status_msg);
 
     // Gửi kết quả xử lý của PCL qua topic output_point_cloud_topic
-   // output_pub_.publish(convertToPointCloud2(clusters[0]));
+    // output_pub_.publish(convertToPointCloud2(clusters[0]));
   }
 
-  void passThroughFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_in,
-                         pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_out)
+  void passThroughFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                         pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_out)
   {
     pcl::PassThrough<pcl::PointXYZ> pass;
     pass.setInputCloud(cloud_in);
@@ -110,8 +124,8 @@ public:
     pass.filter(*cloud_out);
   }
 
-  void voxelGridFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_in,
-                       pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_out)
+  void voxelGridFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                       pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_out)
   {
     pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
     voxel_grid.setInputCloud(cloud_in);
@@ -119,8 +133,8 @@ public:
     voxel_grid.filter(*cloud_out);
   }
 
-  void segmentGround(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_in,
-                     pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_out)
+  void segmentGround(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                     pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_out)
   {
     pcl::SACSegmentation<pcl::PointXYZ> seg;
     seg.setOptimizeCoefficients(true);
@@ -141,9 +155,10 @@ public:
     extract.filter(*cloud_out);
   }
 
-  void euclideanClustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_in,
-                           std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr>& clusters)
+  void euclideanClustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                           std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> &clusters)
   {
+    // Phân nhóm các điểm dữ liệu thành các cụm
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     tree->setInputCloud(cloud_in);
 
@@ -156,47 +171,118 @@ public:
 
     std::vector<pcl::PointIndices> cluster_indices;
     ec.extract(cluster_indices);
-
-    for (const auto& indices : cluster_indices)
+    // Tạo cloud chứa các cụm vật thể
+    int cluster_label = 0;
+    for (const auto &indices : cluster_indices)
     {
       pcl::PointCloud<pcl::PointXYZ>::Ptr cluster(new pcl::PointCloud<pcl::PointXYZ>);
-      for (const auto& index : indices.indices)
+      for (const auto &index : indices.indices)
         cluster->points.push_back(cloud_in->points[index]);
       cluster->width = cluster->points.size();
       cluster->height = 1;
       cluster->is_dense = true;
       clusters.push_back(cluster);
     }
+    cluster_label++;
   }
 
-  void computeSafetyZone(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_in,
-                         pcl::PointCloud<pcl::PointXYZ>::Ptr& safety_warn_cloud,
-                         pcl::PointCloud<pcl::PointXYZ>::Ptr& safety_protect_cloud)
+  void computeSafetyZone(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                         pcl::PointCloud<pcl::PointXYZ>::Ptr &safety_warn_cloud,
+                         pcl::PointCloud<pcl::PointXYZ>::Ptr &safety_protect_cloud)
   {
     pcl::PassThrough<pcl::PointXYZ> pass;
     pass.setInputCloud(cloud_in);
-    pass.setFilterFieldName("z");
+
+    // Vùng cảnh báo
+    pass.setFilterFieldName("x");
     pass.setFilterLimits(safety_warn_position_(0) - safety_warn_size_ / 2,
                          safety_warn_position_(0) + safety_warn_size_ / 2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_warn_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pass.filter(*filtered_warn_cloud);
+    pass.setFilterFieldName("y");
+    pass.setFilterLimits(safety_warn_position_(1) - safety_warn_size_ / 2,
+                         safety_warn_position_(1) + safety_warn_size_ / 2);
     pass.filter(*safety_warn_cloud);
 
+    // Vùng bảo vệ
+    pass.setFilterFieldName("x");
     pass.setFilterLimits(safety_protect_position_(0) - safety_protect_size_ / 2,
                          safety_protect_position_(0) + safety_protect_size_ / 2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_protect_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pass.filter(*filtered_protect_cloud);
+    pass.setFilterFieldName("y");
+    pass.setFilterLimits(safety_protect_position_(1) - safety_protect_size_ / 2,
+                         safety_protect_position_(1) + safety_protect_size_ / 2);
+    pass.filter(*safety_protect_cloud);
+
+    // Lọc dữ liệu theo trục z trong vùng an toàn
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(safety_warn_position_(2) - safety_warn_size_ / 2,
+                         safety_warn_position_(2) + safety_warn_size_ / 2);
+    pass.setInputCloud(filtered_warn_cloud);
+    pass.filter(*safety_warn_cloud);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(safety_protect_position_(2) - safety_protect_size_ / 2,
+                         safety_protect_position_(2) + safety_protect_size_ / 2);
+    pass.setInputCloud(filtered_protect_cloud);
     pass.filter(*safety_protect_cloud);
   }
 
-  std::string computeSafetyStatus(const pcl::PointCloud<pcl::PointXYZ>::Ptr& safety_warn_cloud,
-                                  const pcl::PointCloud<pcl::PointXYZ>::Ptr& safety_protect_cloud)
+  void computeSafetyStatus(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_in,
+                           pcl::PointCloud<pcl::PointXYZ>::Ptr &safety_warn_cloud,
+                           pcl::PointCloud<pcl::PointXYZ>::Ptr &safety_protect_cloud,
+                           std_msgs::String &safety_status_msg)
   {
-    if (safety_protect_cloud->empty())
-      return "Safe";
-    else if (safety_warn_cloud->empty())
-      return "Protect";
+    int num_obstacles = cloud_in->size();
+    if (num_obstacles > 0)
+    {
+      if (num_obstacles > collision_detection_limit_)
+      {
+        safety_status_msg.data = "Collision";
+        return;
+      }
+
+      // Kiểm tra xem các điểm trong vùng an toàn có nằm trong vùng cảnh báo hay vùng bảo vệ không
+      int num_warn_points = 0;
+      int num_protect_points = 0;
+      for (const auto &point : safety_warn_cloud->points)
+      {
+        if (point.z >= safety_warn_position_(2) - safety_warn_size_ / 2 &&
+            point.z <= safety_warn_position_(2) + safety_warn_size_ / 2)
+        {
+          num_warn_points++;
+        }
+      }
+
+      for (const auto &point : safety_protect_cloud->points)
+      {
+        if (point.z >= safety_protect_position_(2) - safety_protect_size_ / 2 &&
+            point.z <= safety_protect_position_(2) + safety_protect_size_ / 2)
+        {
+          num_protect_points++;
+        }
+      }
+
+      if (num_protect_points > 0)
+      {
+        safety_status_msg.data = "Protect";
+      }
+      else if (num_warn_points > 0)
+      {
+        safety_status_msg.data = "Warn";
+      }
+      else
+      {
+        safety_status_msg.data = "Safe";
+      }
+    }
     else
-      return "Warn";
+    {
+      safety_status_msg.data = "Safe";
+    }
   }
 
-  sensor_msgs::PointCloud2 convertToPointCloud2(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+  sensor_msgs::PointCloud2 convertToPointCloud2(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud)
   {
     sensor_msgs::PointCloud2 cloud_msg;
     pcl::toROSMsg(*cloud, cloud_msg);
@@ -223,13 +309,31 @@ private:
   double cluster_tolerance_;
   int min_cluster_size_;
   int max_cluster_size_;
-  double safety_warn_size_;
-  double safety_protect_size_;
+  double collision_threshold_;
+  int collision_detection_limit_;
   Eigen::Vector3d safety_warn_position_;
+  double safety_warn_size_;
   Eigen::Vector3d safety_protect_position_;
+  double safety_protect_size_;
+  std::vector<int> collision_counter_;
+  void displayParameters()
+  {
+    ROS_INFO("Obstacle Detection Parameters:");
+    ROS_INFO("pass_through_z_min: %f", pass_through_z_min_);
+    ROS_INFO("pass_through_z_max: %f", pass_through_z_max_);
+    ROS_INFO("voxel_leaf_size: %f", voxel_leaf_size_);
+    ROS_INFO("ground_seg_distance_threshold: %f", ground_seg_distance_threshold_);
+    ROS_INFO("cluster_tolerance: %f", cluster_tolerance_);
+    ROS_INFO("min_cluster_size: %d", min_cluster_size_);
+    ROS_INFO("max_cluster_size: %d", max_cluster_size_);
+    ROS_INFO("safety_warn_size: %f", safety_warn_size_);
+    ROS_INFO("safety_protect_size: %f", safety_protect_size_);
+    ROS_INFO("safety_warn_position: [%f, %f, %f]", safety_warn_position_(0), safety_warn_position_(1), safety_warn_position_(2));
+    ROS_INFO("safety_protect_position: [%f, %f, %f]", safety_protect_position_(0), safety_protect_position_(1), safety_protect_position_(2));
+  }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "obstacle_detection");
   ObstacleDetection obstacle_detection;
